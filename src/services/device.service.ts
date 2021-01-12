ab37fb5d84509257d34a7b82d54beaf86f3823fc
niach <dennis.straehhuber@gmail.com>
2021-01-12 11:07:00 +0100

    device, location crud

--- src/services/device.service.ts
+++ src/services/device.service.ts
@@ -1,6 +1,35 @@
 import express = require('express');
+import { requireToken } from '../middleware/auth.middleware';
+import { LocationModel } from '../models/location.model';
+import { getDeviceCollection, getLocationCollection } from './mongodb.service';
+import { v4 as uuid } from 'uuid';
+import { locationRoutes } from './location.service';
+import { DeviceModel } from '../models/device.model';
 
 const deviceRoutes = express.Router();
 
+deviceRoutes.post('/device', requireToken, async (req, res) => {
+    const device: DeviceModel = req.body;
+    await getDeviceCollection().insertOne({
+        ...device,
+        _id: uuid()
+    });
+    res.status(200).end();
+});
+
+deviceRoutes.get('/device', requireToken, async (req, res) => {
+    res.json(await getDeviceCollection().find({}));
+});
+
+deviceRoutes.put('/device', requireToken, async (req, res) => {
+    const device: DeviceModel = req.body;
+    await getDeviceCollection().replaceOne({_id: device._id}, device);
+    res.status(200).end();
+});
+
+locationRoutes.delete('/device/:id', requireToken, async (req, res) => {
+    await getDeviceCollection().deleteOne({_id: req.params.id});
+    res.status(200).end();
+});
 
 export {deviceRoutes};
--- src/services/location.service.ts
+++ src/services/location.service.ts
@@ -1,6 +1,34 @@
 import express = require('express');
+import { requireToken } from '../middleware/auth.middleware';
+import { LocationModel } from '../models/location.model';
+import { getLocationCollection } from './mongodb.service';
+import { v4 as uuid} from 'uuid';
 
 const locationRoutes = express.Router();
+
+locationRoutes.post('/location', requireToken, async (req, res) => {
+   const location: LocationModel = req.body;
+   await getLocationCollection().insertOne({
+       ...location,
+       _id: uuid()
+   });
+   res.status(200).end();
+});
+
+locationRoutes.get('/location', requireToken, async (req, res) => {
+    res.json(await getLocationCollection().find({}));
+});
+
+locationRoutes.put('/location', requireToken, async (req, res) => {
+    const location: LocationModel = req.body;
+    await getLocationCollection().replaceOne({_id: location._id}, location);
+    res.status(200).end();
+});
+
+locationRoutes.delete('/location/:id', requireToken, async (req, res) => {
+    await getLocationCollection().deleteOne({_id: req.params.id});
+    res.status(200).end();
+});
 
 
 export {locationRoutes};
