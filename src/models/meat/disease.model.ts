0beb205e5c51f06ea3ecfe0be5ca529d80c7f8ff
niach <niach95@gmail.com>
2021-05-18 10:13:39 +0200

    enhance disease model

--- src/models/meat/disease.model.ts
+++ src/models/meat/disease.model.ts
@@ -1,6 +1,14 @@
 export interface DiseaseModel {
+    diseaseGroup: DiseaseGroup;
     disease: Disease;
     severity: DiseaseSeverity;
+}
+
+export enum DiseaseGroup {
+    Leber = 'LEBER',
+    Lunge = 'LUNGE',
+    Herz = 'HERZ',
+    TK = 'TK'
 }
 
 export enum Disease {
@@ -8,9 +16,45 @@ export enum Disease {
     Perihepatitis = 'PERIHEPATITIS',
     Peritonitis = 'PERITONITIS',
     Fettleber = 'FETTLEBER',
-    Lunge = 'LUNGE',
-    Herz = 'HERZ',
-    TK = 'TK'
+    Pneumonie = 'PNEUMONIE',
+    Pleuritis = 'PLEURITIS',
+    Blutfuelle = 'BLUTFÜLLE',
+    Epicarditis = 'EPICARDITIS',
+    Pericarditits = 'PERICARDITIS',
+    Polyserositis = 'POLYSEROSITIS',
+    Arthritis = 'ARTHRITIS',
+    PSE = 'PSE',
+    HD = 'HD',
+    Abszess = 'ABSZESS',
+    MasSchaden = 'MAS.SCHADEN',
+    Kratzspuren = 'KRATZSPUREN'
+}
+
+export const diseasesForGroup: {[group in DiseaseGroup]: Disease[]} = {
+    [DiseaseGroup.Leber]: [
+        Disease.Milkspots,
+        Disease.Perihepatitis,
+        Disease.Peritonitis,
+        Disease.Fettleber,
+    ],
+   [DiseaseGroup.Lunge]: [
+       Disease.Pneumonie,
+       Disease.Pleuritis,
+       Disease.Blutfuelle
+   ],
+    [DiseaseGroup.Herz]: [
+        Disease.Epicarditis,
+        Disease.Pericarditits,
+        Disease.Polyserositis
+    ],
+    [DiseaseGroup.TK]: [
+        Disease.Arthritis,
+        Disease.PSE,
+        Disease.HD,
+        Disease.Abszess,
+        Disease.MasSchaden,
+        Disease.Kratzspuren
+    ]
 }
 
 export enum DiseaseSeverity {
