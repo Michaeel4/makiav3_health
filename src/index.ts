90512895445c720a1e89666d02a1887fee97f59c
niach <danny@straehhuber.com>
2021-07-01 13:19:26 +0200

    livestream

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import {Server} from './server';
+import { MediaServer, Server } from './server';
 
 console.log('Starting server!');
 
 const server = new Server();
+const mediaServer = new MediaServer();
 setTimeout(async () => {
     await server.start();
+    mediaServer.start();
 }, 0);
--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,11 @@ import { meatRoutes } from './services/meat/meat.routes';
 import { meatStatisticsRoutes } from './services/meat/meat-statistics/meat-statistics.routes';
 import { alertRoutes } from './services/alert/alert.routes';
 import { startHealthChecker } from './services/alert/alert.controller';
+
+const NodeMediaServer = require('node-media-server');
+
+
+
 export class Server {
     private app = express();
 
@@ -55,5 +60,34 @@ export class Server {
         await startHealthChecker();
         console.log('Healthchecker started!');
     }
+
+}
+
+export class MediaServer {
+
+    config = {
+        rtmp: {
+            port: 1935,
+            chunk_size: 60000,
+            gop_cache: true,
+            ping: 30,
+            ping_timeout: 60
+        },
+        http: {
+            port: 8000,
+            allow_origin: '*'
+        }
+    };
+
+    nms: any;
+
+    constructor() {
+        this.nms = new NodeMediaServer(this.config);
+    }
+
+    start() {
+        this.nms.run();
+    }
+
 
 }
