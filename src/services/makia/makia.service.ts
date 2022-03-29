3ba4b96542e87765cc8113465d9c7e47c728582e
niach <danny@straehhuber.com>
2022-03-29 13:55:36 +0200

    dump new

--- src/services/makia/makia.service.ts
+++ src/services/makia/makia.service.ts
@@ -129,7 +129,7 @@ makiaRoutes.post('/makia/entries/filter', requireUser, async (req, res) => {
 
 makiaRoutes.get('/makia/dump_plates', async (req, res) => {
     const rows: MakiaEntry[] = await (getPool().query(
-        'SELECT * FROM entries WHERE id > 780572;'
+        'SELECT * FROM entries WHERE id > 779324;'
     ));
 
     const licensePlates = await getLicensePlates(rows.map(row => row.id));
